54847ee3d5a6ea7ef3d0b35a555b636766ba8225
Michael Penick <michael.penick@datastax.com>
2021-04-28 14:12:32 -0400

    Simplify combined operations

--- restapi/src/test/java/io/stargate/web/docsapi/service/query/condition/impl/CombinedConditionTest.java
+++ restapi/src/test/java/io/stargate/web/docsapi/service/query/condition/impl/CombinedConditionTest.java
@@ -27,7 +27,6 @@ import io.stargate.web.docsapi.service.query.filter.operation.impl.NotInFilterOp
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -52,9 +51,7 @@ class CombinedConditionTest {
           ImmutableCombinedCondition.of(filterOperation, queryValue, true);
 
       assertThat(condition).isNotNull();
-      verify(filterOperation).validateBooleanFilterInput(queryValue);
-      verify(filterOperation).validateStringFilterInput(queryValue);
-      verify(filterOperation).validateDoubleFilterInput(queryValue);
+      verify(filterOperation).validateFilterInput(queryValue);
       verifyNoMoreInteractions(filterOperation);
     }
   }
@@ -129,7 +126,6 @@ class CombinedConditionTest {
     @Test
     public void numberValue() {
       Object filterValue = new Object();
-      when(row.isNull("bool_value")).thenReturn(true);
       when(row.isNull("text_value")).thenReturn(true);
       when(row.isNull("dbl_value")).thenReturn(false);
       when(row.getDouble("dbl_value")).thenReturn(22d);
@@ -145,9 +141,7 @@ class CombinedConditionTest {
     @Test
     public void stringValue() {
       Object filterValue = new Object();
-      when(row.isNull("bool_value")).thenReturn(true);
       when(row.isNull("text_value")).thenReturn(false);
-      when(row.isNull("dbl_value")).thenReturn(true);
       when(row.getString("text_value")).thenReturn("Jordan");
       when(filterOperation.test(filterValue, "Jordan")).thenReturn(true);
 
@@ -162,13 +156,6 @@ class CombinedConditionTest {
   // set of simple int test in order to confirm with existing predicates
   @Nested
   class Integration {
-
-    @BeforeEach
-    public void initRow() {
-      when(row.isNull("bool_value")).thenReturn(true);
-      when(row.isNull("dbl_value")).thenReturn(true);
-      when(row.isNull("text_value")).thenReturn(true);
-    }
 
     @Test
     public void inPredicateNotMatched() {
